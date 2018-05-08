831289bf63f2d5fce24d2fdeee9b2010bffaa95b
kunl <liangkun@tpson.cn>
2018-05-08 10:02:12 +0800

    update server_monitor

--- server_monitor/lk.py
+++ server_monitor/lk.py
@@ -3,17 +3,19 @@ import os
 import time
 import sys
 import re
+import MailUtils
+import socket
 from time import sleep
 
 def main():
     too_busy = 0
-    os.system("top -n 2 -b > tmp")
+    os.system("top -n 2 -b > .tmp")
 #     B:\\tools\\server_monitor\\
-    fin = open("tmp", "r")
+    fin = open(".tmp", "r")
     lines = fin.readlines()  # 读取全部内容  
     fin.close()
     
-    fout = open("top.out", "w")
+    fout = open(".top", "w")
     counter = 0
     for line in lines:
         if line.startswith("%Cpu"):
@@ -43,8 +45,22 @@ def main():
 if __name__ == '__main__':
     while True:        
         too_busy = main()
-        if too_busy == 1:
-            # os.system("mail -s '[AutoDetect]Hacker found!' lk8823@qq.com < top.out")
-            os.system("mail -s '[AutoDetect]Hacker found!' -c zhoulu.luo@coollu.com.cn -c liminhgd@qq.com -c yao.chen@coollu.com.cn -c chao.cao@coollu.com.cn lk8823@qq.com < top.out")
+	if too_busy == 1:
+            myname = socket.getfqdn(socket.gethostname())
+            # 获取本机ip
+            ip = socket.gethostbyname(myname)
+            subject = ip + " cpu占用异常!"
+            if len(sys.argv) > 1
+                subject = sys.argv[1] + " cpu占用异常!"
+            topfile = open('.top');
+            try:
+                toptext = topfile.read();
+            finally:
+                topfile.close();
+
+            MailUtils.sendMail("liuandong@tpson.cn;wuqinghua@tpson.cn;mayuefeng@tpson.cn;liangkun@tpson.cn;", subject, toptext)
+            #MailUtils.sendMail("liangkun@tpson.cn;", subject, toptext)
+            # os.system("mail -s '[AutoDetect]Hacker found!' lk8823@qq.com < .top")
+            #os.system("mail -s '[AutoDetect]Hacker found!' -c liminhgd@qq.com -c yao.chen@coollu.com.cn -c chao.cao@coollu.com.cn lk8823@qq.com < .top")
         sleep(600)
 
