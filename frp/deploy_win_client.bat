681f5564f7f78aeef73b668370f53f536431767e
kunl <liangkun@tpson.cn>
2019-04-10 16:26:27 +0800

    modify scripts

--- frp/deploy_win_client.bat
+++ frp/deploy_win_client.bat
@@ -1,13 +1,8 @@
-:set root=c:\tpsonproxy
-
-set root=E:\Installers\frp\20190409\frp_0.25.3_windows_386
+set root=%~dp0
+echo %root%
+cd /d %~dp0
 :install service
 %root%\nssm.exe install tpson_client %root%\tpson_client.exe -c %root%\tpson_win_client.ini
-
 :start service
 %root%\nssm.exe restart tpson_client
-
-:nssm stop tpson_client
-:nssm restart tpson_client
-:nssm remove tpson_client confirm
 pause
--- frp/remove_client_service.bat
+++ frp/remove_client_service.bat
@@ -1,6 +1,6 @@
-:set root=c:\tpsonproxy
-
-set root=E:\Installers\frp\20190409\frp_0.25.3_windows_386
+set root=%~dp0
+echo %root%
+cd /d %~dp0
 %root%\nssm.exe stop tpson_client
 %root%\nssm.exe remove tpson_client confirm
 pause
