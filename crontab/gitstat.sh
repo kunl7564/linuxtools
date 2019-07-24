bfe367b4f9fbd32aa9e18eccb3042351915fa05d
liangkun <9687564@qq.com>
2019-07-24 12:22:13 +0800

    add year gitstat

--- crontab/gitstat.sh
+++ crontab/gitstat.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DAY=`date +%Y-%m-%d`;
 TIME=`date +%H:%M:%S`;
+YEAR=`date +%Y`;
 MONTH=`date +%Y-%m`;
 WEEK=`date +%u`
 NOW=$DAY'\ '$TIME
@@ -16,12 +17,15 @@ WEEK_END=$NOW
 MONTH_START=$MONTH'-01\ 0:0:0'
 MONTH_END=$NOW
 
+YEAR_START=$YEAR'-01-01\ 0:0:0'
+YEAR_END=$NOW
 
 GERRIT_ROOT=/work/gerrit/gerrit_site/git
 GITSTATS_ROOT=/work/gerrit/gitstats
 GITSTATS_DAY=$GITSTATS_ROOT/day
 GITSTATS_WEEK=$GITSTATS_ROOT/week
 GITSTATS_MONTH=$GITSTATS_ROOT/month
+GITSTATS_YEAR=$GITSTATS_ROOT/year
 sudo mkdir -p $GITSTATS_DAY $GITSTATS_WEEK $GITSTATS_MONTH
 
 ##统计当天
@@ -30,9 +34,13 @@ sudo gitstats -c start_date="$DAY_START" -c end_date="$DAY_END" $GERRIT_ROOT/[^A
 sudo gitstats -c start_date="$WEEK_START" -c end_date="$WEEK_END" $GERRIT_ROOT/[^A]*.git  $GITSTATS_WEEK
 #统计本月
 sudo gitstats -c start_date="$MONTH_START" -c end_date="$MONTH_END" $GERRIT_ROOT/[^A]*.git  $GITSTATS_MONTH
+#统计本年
+sudo gitstats -c start_date="$YEAR_START" -c end_date="$YEAR_END" $GERRIT_ROOT/[^A]*.git  $GITSTATS_YEAR
+
 
 #sudo gitstats -c start_date=2018-3-30 -c end_date=2018-3-30 /work/gerrit/gerrit_site/git/tp_fire_cloud.git tmp
 
 echo $DAY_START $DAY_END
 echo $WEEK_START $WEEK_END
 echo $MONTH_START $MONTH_END
+echo $YEAR_START $YEAR_END
